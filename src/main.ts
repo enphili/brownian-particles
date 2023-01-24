64377cdcc4f515f9cb33220c3d104d6d46adec40
Konst <blurbinfacebook@yandex.ru>
2023-01-24 13:29:45 +0500

    end

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,19 @@
+import { Swiper, SwiperOptions, Parallax, Mousewheel, FreeMode } from 'swiper'
 import './css/main.sass'
 import 'swiper/css'
-import Swiper from 'swiper'
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-<div class="swiper-wrapper slider__wrapper">
-  <div class="swiper-slide slider__item">
-    <div class="slider__layer" data-swiper-parallax="" style="background-image: url(src/img/screen-1/layer-back.jpg)"></div>
-    <div class="slider__layer" data-swiper-parallax="" style="background-image: url(src/img/screen-1/layer-middle.png)"></div>
-    <div class="slider__layer" data-swiper-parallax="14%" style="background-image: url(src/img/screen-1/layer-front.png)"></div>
-  </div>
-
-  <div class="swiper-slide slider__item">
-    <div class="slider__layer" style="background-image: url(src/img/screen-2/layer-back.jpg)"></div>
-    <div class="slider__layer" style="background-image: url(src/img/screen-2/layer-gera.png)"></div>
-    <div class="slider__layer" style="background-image: url(src/img/screen-2/layer-yen.png)"></div>
-    <div class="slider__layer" style="background-image: url(src/img/screen-2/layer-front.png)"></div>
-  </div>
-
-  <div class="swiper-slide slider__item">
-    <div class="slider__layer" style="background-image: url(src/img/screen-3/layer-back.jpg)"></div>
-    <div class="slider__layer" style="background-image: url(src/img/screen-3/layer-middle.png)"></div>
-    <div class="slider__layer" style="background-image: url(src/img/screen-3/layer-ciri.png)"></div>
-    <div class="slider__layer" style="background-image: url(src/img/screen-3/layer-front.png)"></div>
-  </div>
-</div>
-`
-const swiper = new Swiper('.slider', {
+const swiperParams: SwiperOptions = {
+  modules: [ Parallax, Mousewheel, FreeMode ],
+  direction: 'horizontal',
   speed: 2400,
-  mousewheel: true,
-  spaceBetween: 18,
-  parallax: true
-})
+  mousewheel: {
+    enabled: true,
+    sensitivity: 2.4
+  },
+  spaceBetween: 0,
+  parallax: true,
+  freeMode: true
+}
 
-console.log('hfh')
-
+new Swiper('.slider', swiperParams)
 
